d05ff9346079ad6e4a6bf966c823372dd3ecfff4
Peter Graham <pwgraham91@gmail.com>
2019-08-05 22:15:17 -0500

    swap users app to api

--- src/api/Users.kt
+++ src/api/Users.kt
@@ -1,12 +1,9 @@
 package com.instagraham.api
 
 import com.instagraham.API_VERSION
-import com.instagraham.model.User
 import com.instagraham.repository.Repository
 import io.ktor.application.call
 import io.ktor.auth.authenticate
-import io.ktor.auth.authentication
-import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -16,17 +13,8 @@ const val USERS = "$API_VERSION/users"
 fun Route.users(db: Repository) {
     authenticate("auth") {
         get(USERS) {
-            val authenticatedUser = call.authentication.principal as User
             val users = db.users()
-            call.respond(
-                FreeMarkerContent(
-                    "users.ftl",
-                    mapOf(
-                        "users" to users,
-                        "authenticatedUserName" to authenticatedUser.name
-                    )
-                )
-            )
+            call.respond(users.toArray())
         }
     }
 }
--- src/app/Users.kt
+++ src/app/Users.kt
@@ -1,7 +1,10 @@
 package com.instagraham.app
 
+import com.instagraham.model.User
 import com.instagraham.repository.Repository
 import io.ktor.application.call
+import io.ktor.auth.authenticate
+import io.ktor.auth.authentication
 import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -10,8 +13,20 @@ import io.ktor.routing.get
 const val USERS = "/users"
 
 fun Route.users(db: Repository) {
-    get(USERS) {
-        val users = db.users()
-        call.respond(FreeMarkerContent("users.ftl", mapOf("users" to users)))
+    authenticate("auth") {
+        get(USERS) {
+            val users = db.users()
+            val authenticatedUser = call.authentication.principal as User
+
+            call.respond(
+                FreeMarkerContent(
+                    "users.ftl",
+                    mapOf(
+                        "users" to users,
+                        "authenticatedUserName" to authenticatedUser.name
+                    )
+                )
+            )
+        }
     }
 }
